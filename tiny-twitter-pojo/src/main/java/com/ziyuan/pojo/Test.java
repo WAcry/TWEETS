478a79222d768d3c710d9c52d143bff54af66205
Ziyuan Zhang <61440144+WAcry@users.noreply.github.com>
2022-01-24 05:22:43 -0400

    009 add Test Mapper

---
+++ tiny-twitter-pojo/src/main/java/com/ziyuan/pojo/Test.java
@@ -0,0 +1,92 @@
+package com.ziyuan.pojo;
+
+import java.util.Date;
+
+public class Test {
+    private String id;
+
+    private String str;
+
+    private Date datetime;
+
+    private String a;
+
+    private String b;
+
+    private String c;
+
+    private String json;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getStr() {
+        return str;
+    }
+
+    public void setStr(String str) {
+        this.str = str;
+    }
+
+    public Date getDatetime() {
+        return datetime;
+    }
+
+    public void setDatetime(Date datetime) {
+        this.datetime = datetime;
+    }
+
+    public String getA() {
+        return a;
+    }
+
+    public void setA(String a) {
+        this.a = a;
+    }
+
+    public String getB() {
+        return b;
+    }
+
+    public void setB(String b) {
+        this.b = b;
+    }
+
+    public String getC() {
+        return c;
+    }
+
+    public void setC(String c) {
+        this.c = c;
+    }
+
+    public String getJson() {
+        return json;
+    }
+
+    public void setJson(String json) {
+        this.json = json;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getSimpleName());
+        sb.append(" [");
+        sb.append("Hash = ").append(hashCode());
+        sb.append(", id=").append(id);
+        sb.append(", str=").append(str);
+        sb.append(", datetime=").append(datetime);
+        sb.append(", a=").append(a);
+        sb.append(", b=").append(b);
+        sb.append(", c=").append(c);
+        sb.append(", json=").append(json);
+        sb.append("]");
+        return sb.toString();
+    }
+}

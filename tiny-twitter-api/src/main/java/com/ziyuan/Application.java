ee2eff378e12b73c161d8d136b43f1347dd5a7bf
Ziyuan Zhang <61440144+WAcry@users.noreply.github.com>
2022-01-25 05:22:43 -0400

    010 [*important] add mapper scan to detect mapper

--- tiny-twitter-api/src/main/java/com/ziyuan/Application.java
+++ tiny-twitter-api/src/main/java/com/ziyuan/Application.java
@@ -1,9 +1,11 @@
 package com.ziyuan;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(basePackages = "com.ziyuan.mapper")
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);

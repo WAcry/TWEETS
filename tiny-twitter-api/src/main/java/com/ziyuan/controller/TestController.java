13c3e57240d50e375e08a22a23f521e25c933849
Ziyuan Zhang <61440144+WAcry@users.noreply.github.com>
2022-03-12 08:45:43 -0400

    029 Spring Kafka Demo

--- tiny-twitter-api/src/main/java/com/ziyuan/controller/TestController.java
+++ tiny-twitter-api/src/main/java/com/ziyuan/controller/TestController.java
@@ -2,6 +2,7 @@ package com.ziyuan.controller;
 
 import com.ziyuan.pojo.Test;
 import com.ziyuan.service.TestService;
+import com.ziyuan.service.kafka.producer.SpringKafkaProducer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,9 +14,17 @@ public class TestController {
     @Autowired
     private TestService testService;
 
+    @Autowired
+    private SpringKafkaProducer springKafkaProducer;
+
     @PostMapping("/test")
     public void test(@RequestBody Test test) {
         testService.test(test);
+    }
+
+    @PostMapping("/send")
+    public void send() {
+        springKafkaProducer.send("topic_0", "key_0", "value_0");
     }
 }
 
---
+++ tiny-twitter-service/src/main/java/com/ziyuan/service/kafka/consumer/SpringKafkaConsumer.java
@@ -0,0 +1,20 @@
+package com.ziyuan.service.kafka.consumer;
+
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.Acknowledgment;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SpringKafkaConsumer {
+    final static Logger logger = LoggerFactory.getLogger(SpringKafkaConsumer.class);
+
+    @KafkaListener(groupId = "group_0", topics = "topic_0")
+    public void onMessage(ConsumerRecord<String, String> record, Acknowledgment acknowledgment, Consumer<?, ?> consumer) {
+        logger.info("received message: " + record.value());
+        acknowledgment.acknowledge(); // ack
+    }
+}

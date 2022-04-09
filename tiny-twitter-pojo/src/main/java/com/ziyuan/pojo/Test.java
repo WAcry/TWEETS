e8964c272ff1bfe31914b1e12dc62f89db80c950
Ziyuan Zhang <61440144+WAcry@users.noreply.github.com>
2022-04-09 16:27:29 -0400

    037 apply lombok

--- tiny-twitter-pojo/src/main/java/com/ziyuan/pojo/Test.java
+++ tiny-twitter-pojo/src/main/java/com/ziyuan/pojo/Test.java
@@ -1,7 +1,14 @@
 package com.ziyuan.pojo;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.Date;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Test {
     private String id;
 
@@ -16,77 +23,4 @@ public class Test {
     private String c;
 
     private String json;
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getStr() {
-        return str;
-    }
-
-    public void setStr(String str) {
-        this.str = str;
-    }
-
-    public Date getDatetime() {
-        return datetime;
-    }
-
-    public void setDatetime(Date datetime) {
-        this.datetime = datetime;
-    }
-
-    public String getA() {
-        return a;
-    }
-
-    public void setA(String a) {
-        this.a = a;
-    }
-
-    public String getB() {
-        return b;
-    }
-
-    public void setB(String b) {
-        this.b = b;
-    }
-
-    public String getC() {
-        return c;
-    }
-
-    public void setC(String c) {
-        this.c = c;
-    }
-
-    public String getJson() {
-        return json;
-    }
-
-    public void setJson(String json) {
-        this.json = json;
-    }
-
-    @Override
-    public String toString() {
-        StringBuilder sb = new StringBuilder();
-        sb.append(getClass().getSimpleName());
-        sb.append(" [");
-        sb.append("Hash = ").append(hashCode());
-        sb.append(", id=").append(id);
-        sb.append(", str=").append(str);
-        sb.append(", datetime=").append(datetime);
-        sb.append(", a=").append(a);
-        sb.append(", b=").append(b);
-        sb.append(", c=").append(c);
-        sb.append(", json=").append(json);
-        sb.append("]");
-        return sb.toString();
-    }
 }
--- tiny-twitter-pojo/src/main/java/com/ziyuan/pojo/User.java
+++ tiny-twitter-pojo/src/main/java/com/ziyuan/pojo/User.java
@@ -1,5 +1,12 @@
 package com.ziyuan.pojo;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
     private String id;
 
@@ -10,59 +17,4 @@ public class User {
     private String profileImg;
 
     private String ext;
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getProfileImg() {
-        return profileImg;
-    }
-
-    public void setProfileImg(String profileImg) {
-        this.profileImg = profileImg;
-    }
-
-    public String getExt() {
-        return ext;
-    }
-
-    public void setExt(String ext) {
-        this.ext = ext;
-    }
-
-    @Override
-    public String toString() {
-        StringBuilder sb = new StringBuilder();
-        sb.append(getClass().getSimpleName());
-        sb.append(" [");
-        sb.append("Hash = ").append(hashCode());
-        sb.append(", id=").append(id);
-        sb.append(", username=").append(username);
-        sb.append(", password=").append(password);
-        sb.append(", profileImg=").append(profileImg);
-        sb.append(", ext=").append(ext);
-        sb.append("]");
-        return sb.toString();
-    }
 }
